50924b9dcc5f5fc15b143e3310b5d8b2231e79f3
jliu08 <jliu08@uchicago.edu>
2020-04-24 16:18:15 -0400

    adding clink to sequence_three

--- sequence_three.py
+++ sequence_three.py
@@ -1,8 +1,10 @@
 import os
 
-list_of_websites = ['www.youtube.com', 'www.reddit.com', 'www.amazon.com', 'www.amazon.co.jp', 'cnj.craigslist.org']
+list_of_websites = ['www.youtube.com']#, 'www.reddit.com', 'www.amazon.com', 'www.amazon.co.jp', 'cnj.craigslist.org']
 
 f = open("seq3_jliu08.txt", "a")
+
+output = ""
 
 for website in list_of_websites:
     stream = os.popen(  'echo [$(date +"%b %m %H:%M:%S")]\n' + \
@@ -10,10 +12,13 @@ for website in list_of_websites:
                         'echo [$(date +"%b %m %H:%M:%S")]\n' + \
                         'timeout 10 ping ' + website + "\n" + \
                         'echo [$(date +"%b %m %H:%M:%S")]\n' + \
+                        'timeout 180 sudo ./clink.1.0/clink -s64 ' + website + "\n" + \
+                        'echo [$(date +"%b %m %H:%M:%S")]\n' + \
                         'sudo traceroute -I ' + website + "\n" + \
                         'echo [$(date +"%b %m %H:%M:%S")]\n' + \
                         'timeout 10 ping ' + website)
     
-    output = stream.read()
+    output += stream.read()
+    output += "\n"
     
-    f.write(output + "\n")
+f.write(output)
